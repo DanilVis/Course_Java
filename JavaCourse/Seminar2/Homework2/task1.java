44a20be11237d9c99f88e7b7f2e06ab7e82b4b88
Danil <visdanil97@gmail.com>
2022-12-21 02:16:12 +0700

    hw2 task 1

--- JavaCourse/Seminar2/Homework2/task1.java
+++ JavaCourse/Seminar2/Homework2/task1.java
@@ -1,5 +1,8 @@
 package JavaCourse.Seminar2.Homework2;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /*
  * Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
  * используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
@@ -8,6 +11,21 @@ package JavaCourse.Seminar2.Homework2;
  */
 public class task1 {
     public static void main(String[] args) {
+        Map<String, String> students = new LinkedHashMap<>();
+        students.put("name", "Ivanov");
+        students.put("country", "Russia");
+        students.put("city", "Moscow");
+        students.put("age", null);
 
+        String req = "SELECT * FROM students WHERE ";
+
+        StringBuilder where = new StringBuilder();
+        for (Map.Entry<String, String> pair: students.entrySet()) {
+            if (pair.getValue() != null) {
+                where.append(pair.getKey() + " = " + "'" + pair.getValue() + "'" + " AND ");
+            }
+        }
+        where.delete(where.length() - 5, where.length());
+        System.out.println(req + where);
     }
 }
